46a6a59f31e64d07c8e176cec87ee59674cb3390
haikalbasri <basribusrut93@gmail.com>
2023-11-30 05:59:42 +0700

    data type

--- starter/app.js
+++ starter/app.js
@@ -1,2 +1,8 @@
-const projectTitle = document.getElementById('titleh1');
+const projectTitle = document.getElementById("titleh1");
 projectTitle.textContent = "keren sia boy";
+
+let thisFunction = "this is function";
+const PI = 3.14; //karena const valuenya ga bisa diubah
+
+let y,z = 5;
+let x = y + z;
