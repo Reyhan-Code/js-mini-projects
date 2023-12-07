a877b5895d06a4c5408352303767436b5bc6a6b3
haikalbasri <basribusrut93@gmail.com>
2023-12-07 18:58:51 +0700

    perubahan di filter todos

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,63 @@
-const projectTitle = document.getElementById("titleh1");
-projectTitle.textContent = "keren sia boy";
+///kumpulkan semua element ui
 
-let thisFunction = "this is function";
-const PI = 3.14; //karena const valuenya ga bisa diubah
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const filterInput = document.getElementById("filter-input");
+const todoList = document.getElementById("todo-list");
+const clearButton = document.getElementById("clear-todos");
 
-let y,z = 5;
-let x = y + z;
+todoForm.addEventListener("submit", addTodo);
+todoList.addEventListener("click", deleteTodo);
+clearButton.addEventListener("click", clearTodos);
+filterInput.addEventListener("keyup", filterTodos);
+
+function addTodo(e) {
+  e.preventDefault(); //ketika melakukan submit maka akan refresh atau tereload
+  //if dijalankan kalo ada jalanin
+  if (todoInput.value) {
+    //membuat li element
+    const li = document.createElement("li");
+
+    //nambahkan class pada element li
+    li.className =
+      "list-group-item d-flex justify-content-between align-items-center mb-1";
+
+    //nambahin children ke element li
+    li.appendChild(document.createTextNode(todoInput.value)); // dengan append maka
+    //membuat delete button
+    const a = document.createElement("a");
+
+    //memberi properti untuk element a
+    a.href = "#";
+    a.className = "badge badge-danger delete-todo";
+    a.innerHTML = "Delete";
+
+    //menyisipkan element a ke children li
+    li.appendChild(a);
+
+    //memasukkan element li yang telah di buat dengan javascript ke dalam element todoList
+    todoList.appendChild(li);
+    todoInput.value = ""; //mengkosongkan valuenya di kolom input tanpa spasi maka akan jadi hal yang kosong alias clear
+  } //kalo kosong maka tulis kosong
+  else {
+    alert("input tidak boleh kosong, silakan masukan kembali!");
+  }
+}
+
+function deleteTodo(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("delete-todo")) {
+    if (confirm("apakah yakin akan menghapus?")) {
+      const parent = e.target.parentElement;
+      parent.remove();
+    }
+  }
+}
+
+function clearTodos() {
+  todoList.innerHTML = "";
+}
+
+function filterTodos(e) {
+  
+}
